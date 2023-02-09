0ebd76ec2222fba480eae6ff75c4dd6927958d06
skoptelin <warziher@yandex.ru>
2023-02-09 03:13:35 +0300

    first commit

---
+++ app/Http/Controllers/AdController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ad;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
+
+class AdController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(){
+        $ads = DB::table('ads')
+                ->join('users', 'users.id', '=', 'ads.user_id')
+                ->select('ads.id', 'ads.title', 'ads.description', 'ads.price', 'ads.picture', 'users.name', 'users.phone_num', 'ads.city')
+                ->orderBy('ads.updated_at', 'desc')
+                ->get();
+        return Inertia::render('Ads/Index', [
+            'activeAds' => $ads
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ad  $ad
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ad $ad)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Ad  $ad
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ad $ad)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ad  $ad
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ad $ad)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ad  $ad
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ad $ad)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Ads/Index.vue
@@ -0,0 +1,29 @@
+<script>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head } from '@inertiajs/vue3';
+
+    export default {
+        props: {
+            activeAds: Array,
+        },
+
+        components: {
+            Head,
+            AuthenticatedLayout
+        }
+    }
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Список объявлений</h2>
+        </template>
+        ЭТО работает!
+
+        {{ activeAds }}
+
+    </AuthenticatedLayout>
+</template>
